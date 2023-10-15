277d5c4183aa915dee9643cddf7c17f18c4152de
Ryan Rambali <ryanrambali@gmail.com>
2023-10-14 22:42:45 -0400

    layout for login screen

--- lib/Login/LoginScreen.dart
+++ lib/Login/LoginScreen.dart
@@ -7,21 +7,115 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(16, 19, 20, 1),
         centerTitle: true,
         title: Text(
-          "Login Test",
+          "Log In",
           textAlign: TextAlign.center,
-          style: GoogleFonts.notoSans(
-              fontSize: 18,
-              fontWeight: FontWeight.normal
-          ),
+          style:
+              GoogleFonts.notoSans(fontSize: 18, fontWeight: FontWeight.normal),
         ),
       ),
-      backgroundColor: Colors.black,
-
-
+      body: Column(
+        children: [
+          SizedBox(height: 200),
+          const Row(
+            children: [
+              SizedBox(
+                width: 25,
+              ),
+              Expanded(
+                child: TextField(
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromRGBO(51, 54, 63, 1.0)
+                  ),
+                  cursorColor: Color.fromRGBO(51, 54, 63, 1.0),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.email),
+                    prefixIconColor: Color.fromRGBO(51, 54, 63, 1.0),
+                    filled: true,
+                    hintText: "Enter your e-mail here",
+                    hintStyle: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromRGBO(137, 141, 158, 1.0),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 25,
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 25,
+              ),
+              Expanded(
+                child: TextField(
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromRGBO(51, 54, 63, 1.0)
+                  ),
+                  cursorColor: Color.fromRGBO(51, 54, 63, 1.0),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.lock),
+                    prefixIconColor: Color.fromRGBO(51, 54, 63, 1.0),
+                    filled: true,
+                    hintText: "Enter your password here",
+                    hintStyle: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromRGBO(137, 141, 158, 1.0),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 25,
+              )
+            ],
+          ),
+          const SizedBox(height: 20),
+          const Text("Forgot your password?"),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                  child: FloatingActionButton.extended(
+                label: const Text("Let's Start",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.normal)),
+                icon: const Icon(Icons.arrow_forward),
+                backgroundColor: Theme.of(context).primaryColor,
+                onPressed: () {},
+              )),
+            ],
+          ),
+          const SizedBox(height: 20),
+          const Divider(),
+          Row(
+            children: [
+              FloatingActionButton(onPressed: () {}),
+              FloatingActionButton(onPressed: () {})
+            ],
+          )
+        ],
+      ),
     );
   }
 }
