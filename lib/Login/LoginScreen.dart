d920b97dead6312d2d559ca21ffe870aa74b9cdf
Ryan Rambali <ryanrambali@gmail.com>
2023-10-15 04:22:12 -0400

    finished login screen design

--- lib/Login/LoginScreen.dart
+++ lib/Login/LoginScreen.dart
@@ -20,33 +20,14 @@ class LoginScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          SizedBox(height: 200),
+          const Spacer(flex: 1),
           const Row(
             children: [
               SizedBox(
                 width: 25,
               ),
               Expanded(
-                child: TextField(
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
-                      color: Color.fromRGBO(51, 54, 63, 1.0)
-                  ),
-                  cursorColor: Color.fromRGBO(51, 54, 63, 1.0),
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    prefixIcon: Icon(Icons.email),
-                    prefixIconColor: Color.fromRGBO(51, 54, 63, 1.0),
-                    filled: true,
-                    hintText: "Enter your e-mail here",
-                    hintStyle: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
-                      color: Color.fromRGBO(137, 141, 158, 1.0),
-                    ),
-                  ),
-                ),
+                child: EmailField(),
               ),
               SizedBox(
                 width: 25,
@@ -62,27 +43,7 @@ class LoginScreen extends StatelessWidget {
                 width: 25,
               ),
               Expanded(
-                child: TextField(
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
-                      color: Color.fromRGBO(51, 54, 63, 1.0)
-                  ),
-                  cursorColor: Color.fromRGBO(51, 54, 63, 1.0),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    prefixIcon: Icon(Icons.lock),
-                    prefixIconColor: Color.fromRGBO(51, 54, 63, 1.0),
-                    filled: true,
-                    hintText: "Enter your password here",
-                    hintStyle: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
-                      color: Color.fromRGBO(137, 141, 158, 1.0),
-                    ),
-                  ),
-                ),
+                child: PasswordField(),
               ),
               SizedBox(
                 width: 25,
@@ -90,32 +51,163 @@ class LoginScreen extends StatelessWidget {
             ],
           ),
           const SizedBox(height: 20),
-          const Text("Forgot your password?"),
-          const SizedBox(height: 20),
-          Row(
-            children: [
-              Expanded(
-                  child: FloatingActionButton.extended(
-                label: const Text("Let's Start",
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.normal)),
-                icon: const Icon(Icons.arrow_forward),
-                backgroundColor: Theme.of(context).primaryColor,
-                onPressed: () {},
-              )),
-            ],
+          const Text(
+            "Forgot your password?",
+            style: TextStyle(color: Color.fromRGBO(2, 118, 248, 1)),
           ),
           const SizedBox(height: 20),
-          const Divider(),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              FloatingActionButton(onPressed: () {}),
-              FloatingActionButton(onPressed: () {})
+              const SizedBox(width: 25),
+              Expanded(
+                  child: FloatingActionButton.extended(
+                    label: const Text("Sign In",
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(fontSize: 16, fontWeight: FontWeight.normal)),
+                    icon: const Icon(Icons.arrow_forward),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    onPressed: () =>Navigator.of(context).pushNamedAndRemoveUntil('/Home', (route) => false),
+                  )
+                  ),
+              const SizedBox(width: 25),
             ],
-          )
+          ),
+          const SizedBox(height: 10),
+          const Divider(indent: 25,endIndent: 25,),
+          const SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const SizedBox(width: 25,),
+              FloatingActionButton(
+                  backgroundColor: Colors.white,
+                  child: Image.network(
+                      'http://pngimg.com/uploads/google/google_PNG19635.png',
+                      fit:BoxFit.cover
+                  ),
+                  onPressed: () {}),
+              FloatingActionButton(
+                  backgroundColor: Colors.white,
+                  child: Image.network(
+                      'https://pngimg.com/uploads/apple_logo/apple_logo_PNG19682.png',
+                      height: 40,
+                      fit:BoxFit.cover
+                  ),
+                  onPressed: () {}),
+              const SizedBox(width: 25,),
+            ],
+          ),
+          const Spacer(flex: 2),
         ],
+
       ),
     );
   }
 }
+
+class PasswordField extends StatefulWidget {
+  const PasswordField({
+    super.key,
+  });
+
+  @override
+  State<PasswordField> createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  final textFieldFocusNode = FocusNode();
+  bool _obscured = true;
+
+  void _toggleObscured() {
+    setState(() {
+      _obscured = !_obscured;
+      if (textFieldFocusNode.hasPrimaryFocus) return; // If focus is on text field, dont unfocus
+      textFieldFocusNode.canRequestFocus = false;     // Prevents focus if tap on eye
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.visiblePassword,
+      focusNode: textFieldFocusNode,
+      obscureText: _obscured,
+      style: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+          color: Color.fromRGBO(51, 54, 63, 1.0)
+      ),
+      cursorColor: const Color.fromRGBO(51, 54, 63, 1.0),
+      decoration: InputDecoration(
+        floatingLabelBehavior: FloatingLabelBehavior.never, //Hides label on focus or if filled
+        isDense: true,  // Reduces height a bit
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,              // No border
+          borderRadius: BorderRadius.circular(14.0),
+        ),
+        fillColor: Colors.white,
+        prefixIcon: const Icon(Icons.lock_rounded),
+        prefixIconColor: const Color.fromRGBO(51, 54, 63, 1.0),
+        suffixIconColor: const Color.fromRGBO(51, 54, 63, 1.0),
+        suffixIcon: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 4, 0),
+          child: GestureDetector(
+            onTap: _toggleObscured,
+            child: Icon(
+              _obscured
+                  ? Icons.visibility_rounded
+                  : Icons.visibility_off_rounded,
+              size: 24,
+            ),
+          ),
+        ),
+        filled: true,
+        hintText: "Enter your password",
+        hintStyle: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+          color: Color.fromRGBO(137, 141, 158, 1.0),
+        ),
+      ),
+    );
+  }
+}
+
+class EmailField extends StatelessWidget {
+  const EmailField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      style: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+          color: Color.fromRGBO(51, 54, 63, 1.0)
+      ),
+      cursorColor: const Color.fromRGBO(51, 54, 63, 1.0),
+      decoration: InputDecoration(
+        isDense: true,  // Reduces height a bit
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,              // No border
+          borderRadius: BorderRadius.circular(14.0),
+        ),
+        fillColor: Colors.white,
+        prefixIcon: const Icon(Icons.email),
+        prefixIconColor: const Color.fromRGBO(51, 54, 63, 1.0),
+        filled: true,
+        hintText: "Enter your e-mail",
+        hintStyle: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+          color: Color.fromRGBO(137, 141, 158, 1.0),
+        ),
+      ),
+    );
+  }
+}
+
+
